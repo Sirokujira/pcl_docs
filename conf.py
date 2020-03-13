abda3d334c902f08f0a0efbd13d1d7e65c275488
sirokujira <toru753@gmail.com>
2020-03-13 11:24:50 +0900

    modified conf.py

--- conf.py
+++ conf.py
@@ -163,8 +163,9 @@ def setup(app):
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 if read_the_docs_build:
     print("--- read the docs build ---")
-    read_the_docs_version_name = os.environ.get('READTHEDOCS_VERSION', 'latest')
-    rootpath = os.path.join(os.getcwd(), 'conda', read_the_docs_version_name)
+    #read_the_docs_version_name = os.environ.get('READTHEDOCS_VERSION', 'latest')
+    #rootpath = os.path.join(os.getcwd(), 'conda', read_the_docs_version_name)
+    rootpath = os.environ.get('CONDA_PREFIX')
     os.environ["EIGEN_INCLUDE_DIR"] = os.path.join(rootpath, 'include/eigen3')
     os.environ["EIGEN3_INCLUDE_DIR"] = os.path.join(rootpath, 'include/eigen3')
     os.environ["EIGEN_ROOT"] = os.path.join(rootpath, 'include/eigen3')
